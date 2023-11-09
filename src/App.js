cc2a3a3075399da53789654fa2a80693d4410ed4
lenrich <lenrich1403@gmail.com>
2023-11-10 01:28:30 +0800

    014-010. Lifecycle Methods

--- src/App.js
+++ src/App.js
@@ -34,11 +34,12 @@ function formatDay(dateStr) {
 
 export default class App extends React.Component {
   state = {
-    location: "lisbon",
+    location: "",
     isLoading: false,
     displayLocation: "",
     weather: {},
   };
+
   // 如果使用箭頭函數，就可以不用bind 跟建構函數了
   // constructor(props) {
   //   super(props);
@@ -49,6 +50,7 @@ export default class App extends React.Component {
   // async fetchWeather() {
   // 我們使用箭頭函數就可以不用bind method了
   fetchWeather = async () => {
+    if (this.state.location.length < 2) return this.setState({ weather: {} });
     try {
       this.setState({ isLoading: true });
       // 1) Getting location (geocoding)
@@ -73,19 +75,38 @@ export default class App extends React.Component {
       const weatherData = await weatherRes.json();
       this.setState({ weather: weatherData.daily });
     } catch (err) {
-      console.log(err);
+      console.error(err);
     } finally {
       this.setState({ isLoading: false });
     }
-  }
+  };
 
   setLocation = (e) => this.setState({ location: e.target.value });
+
+  // 在component載入的時候初始化的動作
+  // useEffect(()=> {}, [])
+  componentDidMount() {
+    // 一旦component載入，就立刻執行
+    // this.fetchWeather();
+    this.setState({ location: localStorage.getItem("location") || "" });
+  }
+
+  // useEffect(()=>{}, [location])
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.location !== prevState.location) {
+      this.fetchWeather();
+    }
+    localStorage.setItem("location", this.state.location);
+  }
 
   render() {
     return (
       <div className="app">
         <h1>Classy Weather</h1>
-        <Input location={this.state.location} onChangeLocation={this.setLocation}/>
+        <Input
+          location={this.state.location}
+          onChangeLocation={this.setLocation}
+        />
         {/* <div>
           <input
             type="text"
@@ -96,7 +117,7 @@ export default class App extends React.Component {
             }}
           ></input>
         </div> */}
-        <button onClick={this.fetchWeather}>Get Weather</button>
+        {/* <button onClick={this.fetchWeather}>Get Weather</button> */}
 
         {this.state.isLoading && <p className="loader">Loading...</p>}
         {this.state.weather.weathercode?.length > 0 && (
@@ -126,13 +147,33 @@ class Input extends React.Component {
 }
 
 class Weather extends React.Component {
+  // like useEffect cleaning up function
+  // 這裡是真實組件被卸載之後運行，在組件消失並且被銷毀之後，而不是在渲染之間
+  componentWillUnmount() {
+    console.log('Weather will unmount')
+  }
+
   render() {
-    const { temperature_2m_max: max, temperature_2m_min: min, time: dates, weathercode: codes } = this.props.weather;
+    const {
+      temperature_2m_max: max,
+      temperature_2m_min: min,
+      time: dates,
+      weathercode: codes,
+    } = this.props.weather;
     return (
       <div>
         <h2>Weather {this.props.location}</h2>
         <ul className="weather">
-          {dates.map((date, i)=> <Day isToday={i === 0} key={date} date={date} max={max.at(i)} min={min.at(i)} code={codes.at(i)}/>)}
+          {dates.map((date, i) => (
+            <Day
+              isToday={i === 0}
+              key={date}
+              date={date}
+              max={max.at(i)}
+              min={min.at(i)}
+              code={codes.at(i)}
+            />
+          ))}
         </ul>
       </div>
     );
@@ -141,13 +182,15 @@ class Weather extends React.Component {
 
 class Day extends React.Component {
   render() {
-    const {date, max, min, code, isToday} = this.props;
-    return(
+    const { date, max, min, code, isToday } = this.props;
+    return (
       <li className="day">
         <span>{getWeatherIcon(code)}</span>
-        <p>{ isToday ? 'Today' : formatDay(date)}</p>
-        <p>{Math.floor(min)}&deg; &mdash; <strong>{Math.ceil(max)}&deg;</strong></p>
+        <p>{isToday ? "Today" : formatDay(date)}</p>
+        <p>
+          {Math.floor(min)}&deg; &mdash; <strong>{Math.ceil(max)}&deg;</strong>
+        </p>
       </li>
-    )
+    );
   }
 }
